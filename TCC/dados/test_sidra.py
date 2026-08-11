8bf5e850a0544ae3fbaea52193e0a78a40402717
LucasBarreto29 <lucaslbarreto05@gmail.com>
2026-08-11 17:45:41 -0300

    Remove TCC: agora em repositório próprio

--- TCC/dados/test_sidra.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://servicodados.ibge.gov.br/api/v3/agregados?busca=grupamento"
-resp = requests.get(url)
-for r in resp.json():
-    for agg in r['agregados']:
-        if 'PNAD Cont' in r['nome'] or 'PNAD Cont' in agg['nome']:
-             print(f"Table {agg['id']}: {agg['nome']}")
